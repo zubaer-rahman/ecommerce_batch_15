3b686f13244a44312b3e0db77e882524d19c03fb
zubaer-rahman <zubaer.rahman.cse@gmail.com>
2022-12-15 23:19:13 +0600

    both bakcend and frontend mastering are done initially

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EcommerceController;
+use App\Http\Controllers\adminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', [EcommerceController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', [adminController::class, 'index'])->name('dashboard');
+});
